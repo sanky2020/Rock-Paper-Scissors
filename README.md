aa9dba3968a79e80ffc541ddd45f0b4cb6140f01
sanky2020 <69984129+sanky2020@users.noreply.github.com>
2020-12-20 17:31:15 +0530

    Add files via upload
    Created UI for the Game

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-Wrote a logic for the game ROCK, PAPER , & SCISSOR.
+Wrote a logic for the game ROCK, PAPER , & SCISSOR with UserInterface to select the option rather than typing it manually.
 
 The task was very interesting as we had to implement all that we have learnt so far.
-Created a Welcome Message for this GAME in the form of Alert.
-Below is the step by step list of the actions taken:
+Below is the step by step list of the actions taken;
 
 1) Firstly created a function for the computer to give random inputs as per the game requirement by calling respective Random numbers from (1 to 3) and assigning them with their particular values, that is;
  1 = ROCK, 2 = PAPER, & 3 = SCISSOR respectively.
 
-2) Declared variables win, lose, & tie globally and defined them with 0.
+2) Declared variables win, lose, & tie globally and initialized them with 0.
 
-3) Wrote a function "playRound" with 2 parameters ( 1st from user [by taking inputs via prompt from user 5 times and converting it to case insensitive inorder to use it further in accordance with the program logic], and 2nd from computer) and then comparing both inputs with each other as per Game's Rule, and analyzed to check for the  Game (won/lost/tie) and gave the respective return value by incrementing its particular variable(win/lose/tie).
+3) Wrote a function "playRound" with 2 arguments ( 1st from user, 2nd from computer) then comparing both inputs with each other as per Game's Rule.
 
-4) Wrote the main function and gave it a name as "game()". Wrote a for loop in it for looping the same function "playRound" 5 times and displaying the result at the end of each round. Finally displayed total number of Games Won, Games Lost and Games Tied based on which the Final Result is displayed whether you Won/Lost/ or the Game Tied.
+4) Analyzed to check for the  Game (won/lost/tie) and displaying their respective count after each round.
+
+5) Unlike earlier method to display everything using console.log, this time used "innerHTML method" to display the output result directly on the webpage.
+
+6) Designed the logic for this game such that, Winner will be the one reaching early 5 points. 
---
+++ ui.js
@@ -0,0 +1,97 @@
+function computerPlay(){ //function for the computer to select (rock/paper/scissor) randomly
+
+        let random = Math.floor((Math.random()*3)); //returns random number between 1 to 3
+
+        if (random==1){ // assigned no.1 to "rock"
+            return "rock";
+        }
+
+        else if (random==2){ //assigned no.2 to "paper"
+            return "paper";
+        }
+
+        else{ return "scissor";} //assigned the remaining number to "scissor"
+    }
+
+    let win=0,lose=0,tie=0,result,display; //defining variables globally (win,lose,tie) and declaring them with the value 0
+    
+    function playRound(playerSelection, computerSelection) {
+
+        if( playerSelection=="rock" && computerSelection=="scissor" || //Setting up game's rule.
+        playerSelection=="paper" && computerSelection=="rock" || 
+        playerSelection=="scissor" && computerSelection=="paper"){ 
+            win++; 
+            result= ("You selected"+" "+playerSelection+ " & Computer selected"+" "+computerSelection+ "<br><br> " + playerSelection+ " beats "+ computerSelection+"<br><br>"+ "Player = "+ win +", "+"Computer = "+lose+", "+" Tie = "+tie);
+            document.getElementById('result').innerHTML= result;
+            return;
+        }
+        else if(playerSelection==computerSelection){ 
+            tie++;
+            result= ("You both Selected"+" "+playerSelection+ "<br><br> " + " Its a Tie "+"<br><br>"+ "Player = "+ win +", "+"Computer = "+lose+", "+" Tie = "+tie);
+            document.getElementById('result').innerHTML= result;
+            return;
+        }
+        else{
+            lose++; 
+            result= ("You selected"+" "+playerSelection+ " & Computer selected"+" "+computerSelection+ "<br><br> " + computerSelection+ " beats "+ playerSelection+"<br><br>"+ "Player = "+ win +", "+"Computer = "+lose+", "+" Tie = "+tie);
+            document.getElementById('result').innerHTML= result;
+            return;
+        }        
+}
+        // const container=document.querySelector('#container');
+        // const div=document.createElement('div');
+        // container.appendChild(div);
+        
+
+        const rock= document.querySelector('#rock');
+        rock.addEventListener('click',()=>{
+            playerOption="rock";
+            game();
+        });
+
+        const paper= document.querySelector('#paper');
+        paper.addEventListener('click',()=>{
+            playerOption="paper";
+            game();
+        });
+
+        const scissor= document.querySelector('#scissor');
+        scissor.addEventListener('click',()=>{
+            playerOption="scissor";
+            game();
+        });
+
+        const reset= document.querySelector('#reset');
+        reset.addEventListener('click',() =>{
+            win=0,lose=0,tie=0;
+        });
+
+        const playagain= document.querySelector('#playagain');
+        playagain.addEventListener('click',() =>{
+            win=0,lose=0,tie=0;
+        });
+    
+function game(){ //main function ()
+
+let playerSelection = playerOption; 
+let computerSelection = computerPlay(); //adding computer selected value to variable computerSelection
+playRound(playerSelection,computerSelection); 
+
+if(win==5 ||lose==5){
+    alert('Game over! Press Ok to see the result');
+    
+    console.log("Number of Game Won"+" = "+win); //displays winning count
+    console.log("Number of Game Lost"+" = "+lose); //displays lost count
+    console.log("Number of Game Tied"+" = "+tie); //diaplays game tied count
+
+    if(win>lose){       //applying condition to decide whether you won or lost
+        result +=("<br><br>"+"Result : YOU WON");
+} 
+
+else {    
+    result +=("<br><br>"+"Result : YOU LOST");
+}
+document.getElementById('result').innerHTML= result;
+return;
+}
+}
